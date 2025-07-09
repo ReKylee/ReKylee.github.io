dde52fe6f47914eab288daf93c839afb3ca5d59a
ReKylee <ido5490@gmail.com>
2025-07-09 23:36:41 +0300

    Updated action

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,20 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [vitePreprocess()],
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '404.html',
+      precompress: false,
+      strict: true
+    }),
+    paths: {
+      base: process.env.BASE_PATH
+    }
   },
 };
 
